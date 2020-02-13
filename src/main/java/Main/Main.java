13e38a463efd3df3a67057f692dd07c63e3b04dc
aavelinee <eileen.jamali@gmail.com>
2020-02-13 17:55:22 +0330

    add javalin dependencies

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,66 +1,15 @@
 package Main;
 
 import Domain.Loghmeh;
+import io.javalin.Javalin;
 
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args){
-//        String input = "{\"name\": \"gheime\", \"description\": \"it's yummy!\", \"popularity\": 0.8, \"restaurantName\": \"Hesturan\", \"price\": 20000}";
-        Loghmeh loghmeh = new Loghmeh();
-        Scanner input = new Scanner(System.in);
-        String inputCommand = input.nextLine();
-        String command;
-        String result;
-        while(!inputCommand.equals("quit")){
-            if(inputCommand.contains(" "))
-                command = inputCommand.substring(0, inputCommand.indexOf(' '));
-            else
-                command = inputCommand;
-
-            if(command.equals("addRestaurant")){
-                String jsonInput = inputCommand.substring(inputCommand.indexOf(' ') + 1);
-                result = loghmeh.addRestaurant(jsonInput);
-            }
-            else if(command.equals("addFood")){
-                String jsonInput = inputCommand.substring(inputCommand.indexOf(' ') + 1);
-                result = loghmeh.addFoodToRestaurant(jsonInput);
-            }
-            else if(command.equals("getRestaurants"))
-                result = loghmeh.getRestaurants();
-            else if(command.equals("getRestaurant"))
-            {
-                String jsonInput = inputCommand.substring(inputCommand.indexOf(' ') + 1);
-                result = loghmeh.getRestaurant(jsonInput);
-            }
-            else if(command.equals("getFood")){
-                String jsonInput = inputCommand.substring(inputCommand.indexOf(' ') + 1);
-                result = loghmeh.getFoodFromRestaurant(jsonInput);
-            }
-            else if(command.equals("addToCart")){
-                String jsonInput = inputCommand.substring(inputCommand.indexOf(' ') + 1);
-                result = loghmeh.addToCart(jsonInput);
-            }
-            else if(command.equals("getCart")){
-                result = loghmeh.getCart();
-            }
-            else if(command.equals("finalizeOrder")){
-                result = loghmeh.finalizeOrder();
-            }
-            else if(command.equals("getRecommendedRestaurants")){
-                result = loghmeh.getRecommendedRestaurants();
-            }
-            else if(command.equals("quit")){
-                result = "Goodbye";
-                break;
-            }
-            else
-                result = "command not found";
-
-            System.out.println(result);
-            inputCommand = input.nextLine();
-        }
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
 
