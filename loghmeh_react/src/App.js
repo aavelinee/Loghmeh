bddca757d0b11f5e5eae0e19c01e91ae7222da28
Aylin <aylin.jamali@snapp.cab>
2020-04-06 18:39:19 +0430

    Add sign pages

--- loghmeh_react/src/App.js
+++ loghmeh_react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './common/Navbar';
 import Footer from './common/Footer';
+import Sign from './sign/Sign';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Sign />
         <Footer />
       </div>
     );
--- loghmeh_react/src/sign/Register.js
+++ loghmeh_react/src/sign/Register.js
@@ -1,38 +1,43 @@
-class RegisterPhoto extends React.Component {
-	render() {
+import SignInForm from './SignIn/SignIn';
+import SignUpForm from './SignUp/SignUp';
+import { Component } from 'react';
+
+class Register extends Component {
+	RegisterPhoto() {
 		return (
 			<div className="jumbotron">
 				<div className="background-image"></div>
 			</div>
 		);
 	}
+	
+	Register (props) {
+		const isSignUp = props.isSignUp;
+		if (isSignUp) {
+		  return(
+			<div className = "main-content">
+			  <div className = "sign-up-box">
+				<div className = "sign-up-header"><h2> ثبت نام </h2></div>
+				<div className = "sign-up-form">
+				<SignUpForm />
+				</div>
+			  </div>
+			</div>
+		  );
+		}
+		else {
+		  return(
+			<div className = "main-content">
+			  <div className = "sign-in-box">
+				<div className = "sign-in-header"><h2> ورود </h2></div>
+				<div className = "sign-in-form">
+				<SignInForm />
+				</div>
+			  </div>
+			</div>
+		  );
+		}
+	}	
 }
 
-
-function Register (props) {
-	const isSignUp = props.isSignUp;
-	if (isSignUp) {
-	  return(
-		<div className = "main-content">
-		  <div className = "sign-up-box">
-			<div className = "sign-up-header"><h2> ثبت نام </h2></div>
-			<div className = "sign-up-form">
-			<SignUpForm />
-			</div>
-		  </div>
-		</div>
-	  );
-	}
-	else {
-	  return(
-		<div className = "main-content">
-		  <div className = "sign-in-box">
-			<div className = "sign-in-header"><h2> ورود </h2></div>
-			<div className = "sign-in-form">
-			<SignInForm />
-			</div>
-		  </div>
-		</div>
-	  );
-	}
-}
+export default Register;
