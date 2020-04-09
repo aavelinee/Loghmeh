5e473ab1cfa1afc6c67a7445c501409955778e56
Aylin <aylin.jamali@snapp.cab>
2020-04-09 10:58:45 +0430

    Change App

--- loghmeh_react/src/App.js
+++ loghmeh_react/src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import './App.css';
 
 import Sign from './sign/Sign';
 import Home from './Home/Home'
+import Menu from './menu/Menu';
+import { render } from 'react-dom';
+import Example from './common/Modal';
+import Navbar from './common/Navbar';
+import FoodPartyFood from './Home/foodparty/FoodPartyFood';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-        {/* <Sign isSignUp={false}/> */}
+      <div>
+          {/* <Menu /> */}
+          {/* <Sign isSignUp={false}/> */}
+        <Example test={<FoodPartyFood logo={true}/>}/>
       </div>
     );
   }
