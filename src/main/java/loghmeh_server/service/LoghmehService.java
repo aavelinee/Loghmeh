6753763006d471ed30dbc7426f083bbba8756ec4
BaharanKh <BaharanKh@gitlab.com>
2020-04-10 10:14:25 +0430

    Finding cors isue

--- src/main/java/loghmeh_server/service/LoghmehService.java
+++ src/main/java/loghmeh_server/service/LoghmehService.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.rmi.runtime.Log;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
