0a5dc0f14e6f7fb7a8f3db5a8ed05494c303122d
BaharanKh <BaharanKh@gitlab.com>
2020-05-08 22:49:05 +0430

    Implement ui signin

--- src/main/java/loghmeh_server/security/JWTUtils.java
+++ src/main/java/loghmeh_server/security/JWTUtils.java
@@ -2,18 +2,8 @@ package loghmeh_server.security;
 
 
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Encoders;
-import io.jsonwebtoken.security.Keys;
 import loghmeh_server.repository.customer.Customer;
-import loghmeh_server.repository.customer.CustomerMapper;
-
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
-import java.util.Map;
 
 
 import static loghmeh_server.security.SecurityConstants.*;
@@ -29,20 +19,11 @@ public class JWTUtils {
     }
 
     public String generateJWTToken(Customer customer) {
-
-//        try {
-
-
-//        String base64Secret = Base64.getEncoder().encodeToString(SECRET.getBytes());
-//        byte[] secretBytes = DatatypeConverter.parseBase64Binary(base64Secret);
-//        Key key = new SecretKeySpec(secretBytes, SignatureAlgorithm.HS256.getJcaName());
-
-
         System.out.println("key: "+SECRET_KEY);
 
         String token = Jwts.builder()
                     .setHeaderParam("typ", "JWT")
-//                .setSubject(customer.getFirstName() + " " + customer.getLastName())
+                    .setSubject(customer.getFirstName() + " " + customer.getLastName())
                     .setIssuedAt(new Date(System.currentTimeMillis()))//iat
                     .setIssuer(SecurityConstants.TOKEN_ISSUER)//iss
                     .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))//exp
@@ -51,12 +32,6 @@ public class JWTUtils {
                     .compact();
             System.out.println("jwt token: " + token);
             return token;
-//
-//        }
-//        catch(UnsupportedEncodingException uee){
-//            System.out.println(" UnsupportedEncodingException in generating key");
-//            return null;
-//        }
     }
 
     public int verifyJWTToken(String token) {
@@ -74,10 +49,6 @@ public class JWTUtils {
             System.out.println(ex);
             return -1;
         }
-//        catch(UnsupportedEncodingException uee){
-//            System.out.println(" UnsupportedEncodingException in generating key");
-//            return -1;
-//        }
     }
 
 }
